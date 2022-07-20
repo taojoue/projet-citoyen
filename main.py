13356d4501349b8dfeb833b0cb3d84442806a373
GenieFaire <jennifer.cavaccuiti@yahoo.fr>
2022-07-20 15:13:26 +0200

    feat : compt le nombre de cas apr mois

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ import csv
 PathFileCompany = "./Data/fileCompany.csv"
 PathFileCovid = "./Data/fileCovid.csv"
 
-col_list = ["date","tx_pos","tx_incid","TO","R","rea","hosp","rad","dchosp","incid_rea","incid_hosp","incid_rad","incid_dchosp","conf","conf_j1","pos","esms_dc","dc_tot","pos_7j","cv_dose1","esms_cas"]
+col_list = ["date", "tx_pos", "tx_incid", "TO", "R", "rea", "hosp", "rad", "dchosp", "incid_rea", "incid_hosp",
+            "incid_rad", "incid_dchosp", "conf", "conf_j1", "pos", "esms_dc", "dc_tot", "pos_7j", "cv_dose1",
+            "esms_cas"]
 df = pd.read_csv(PathFileCovid, sep=',', usecols=col_list, encoding='utf-8')
+df['date'] = pd.to_datetime(df['date'], errors='coerce')
 
-print('Nombre d éléves :', (df['Effectif']).sum())
+df.set_index('date').resample('M')["pos"].sum()
+
+print(df.set_index('date').resample('M')["pos"].sum())
+
 
 # with open(PathFileCompany, "r") as fichier:
 
