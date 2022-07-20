10667997874bc5ff086c04b2c3a945ddd99f9b52
Tao Jouet <49972956+taojoue@users.noreply.github.com>
2022-07-20 14:00:46 +0200

    Add covid data and logic (#2)
    * Add first changes
    
    * Add fileCovid and logic

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import pandas as pd
 import csv
 
-Path = "./file.csv"
+PathFileCompany = "./Data/fileCompany.csv"
+PathFileCovid = "./Data/fileCovid.csv"
 
-with open(Path, "r") as fichier:
+col_list = ["date","tx_pos","tx_incid","TO","R","rea","hosp","rad","dchosp","incid_rea","incid_hosp","incid_rad","incid_dchosp","conf","conf_j1","pos","esms_dc","dc_tot","pos_7j","cv_dose1","esms_cas"]
+df = pd.read_csv(PathFileCovid, sep=',', usecols=col_list, encoding='utf-8')
 
-    csv_reader = csv.reader(fichier, delimiter=",")
+print('Nombre d éléves :', (df['Effectif']).sum())
 
-    for ligne in csv_reader:
-        print(ligne)
+# with open(PathFileCompany, "r") as fichier:
+
+#     csv_reader = csv.reader(fichier, delimiter=",")
+
+#     for ligne in csv_reader:
+#         print(ligne)
 
 # siren,nic,siret,dateFin,dateDebut,etatAdministratifEtablissement,changementEtatAdministratifEtablissement,enseigne1Etablissement,enseigne2Etablissement,enseigne3Etablissement,changementEnseigneEtablissement,denominationUsuelleEtablissement,changementDenominationUsuelleEtablissement,activitePrincipaleEtablissement,nomenclatureActivitePrincipaleEtablissement,changementActivitePrincipaleEtablissement,caractereEmployeurEtablissement,changementCaractereEmployeurEtablissement
+# date,tx_pos,tx_incid,TO,R,rea,hosp,rad,dchosp,incid_rea,incid_hosp,incid_rad,incid_dchosp,conf,conf_j1,pos,esms_dc,dc_tot,pos_7j,cv_dose1,esms_cas
